51f77da2696a076ee215e08e209a3e7cad25102f
luffytaro19 <tonnytang13@gmail.com>
2024-06-08 23:43:26 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+import discord
+import os
+from discord.ext import commands
+from keep_alive import keep_alive
+
+client = commands.Bot(command_prefix='&' , intents=discord.Intents.all())
+
+@client.event
+async def on_ready():
+  await client.change_presence(activity=discord.Game('Yui'))
+  print('Logged in as {0.user}'.format(client))
+
+@client.command(aliases=['p', 'q'])
+async def ping(ctx, arg=None):
+  if arg == "pong":
+    await ctx.send('hello')
+
+  else:
+    await ctx.send(f'Pong: {round(client.latency * 1000)}ms')
+
+keep_alive()
+client.run(os.getenv('TOKEN'))
